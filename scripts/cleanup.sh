6f44d75c95ab41059e9048c693f842ebca0169ca
skoenig <soeren.koenig@mailbox.org>
2022-11-11 19:26:50 +0200

    lock default build user

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 
+# cleanup
 apt-get -y purge manpages man-db info vim-runtime
 apt-get -y autoremove --purge
 apt-get -y clean
 
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/cache/apt/*
+
+# lock default user
+passwd --lock debian
